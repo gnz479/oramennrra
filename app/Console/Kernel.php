52f5fc2afb470ea6ca455cc00521713f1df7de75
gnz479 <1gonzamessi@gmail.com>
2024-10-30 15:28:38 -0600

    first commit

---
+++ app/Console/Kernel.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\Log;
+use App\Http\Controllers\Controller;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     */
+    protected function schedule(Schedule $schedule): void
+    {
+        // $schedule->command('inspire')->hourly();
+        $schedule->call(function () {
+            try {
+                $controlador->store();
+                \Log::info('Tarea programada ejecutada correctamente.');
+            } catch (\Exception $e) {
+                \Log::error('Error al ejecutar la tarea programada: ' . $e->getMessage());
+            }
+        // })->everyMinute(); 
+        })->dailyAt('17:01'); 
+        
+    }
+
+    /**
+     * Register the commands for the application.
+     */
+    protected function commands(): void
+    {
+        $this->load(__DIR__.'/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
---
+++ resources/views/test.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEST</title>
+</head>
+<body>
+    <h1>Vista de prueba</h1>
+    <p>{{ date('d-m-Y H:i:s')}}</p>
+</body>
+</html>
