08ab25fa10d3dc3a6f0e204696d3c9e74ff59bb5
gnz479 <1gonzamessi@gmail.com>
2024-11-04 11:02:24 -0600

    Kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,10 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 use Illuminate\Support\Facades\Log;
-<<<<<<< HEAD
-=======
 use App\Http\Controllers\Email\EmailController;
->>>>>>> 4e16e7f861f7d876825b05331e2fb39cdc0a8ca2
 
 class Kernel extends ConsoleKernel
 {
@@ -20,23 +17,16 @@ class Kernel extends ConsoleKernel
         // $schedule->command('inspire')->hourly();
         $schedule->call(function () {
             try {
-<<<<<<< HEAD
-                //$controlador->store();
-=======
+
                 $controlador = new EmailController();
                 $controlador->sendEmail();
->>>>>>> 4e16e7f861f7d876825b05331e2fb39cdc0a8ca2
                 \Log::info('Tarea programada ejecutada correctamente.');
             } catch (\Exception $e) {
                 \Log::error('Error al ejecutar la tarea programada: ' . $e->getMessage());
             }
-<<<<<<< HEAD
-        //})->everyMinute(); 
-        })->dailyAt('16:10'); 
-=======
+
         })->everyMinute(); 
         // })->dailyAt('08:45'); 
->>>>>>> 4e16e7f861f7d876825b05331e2fb39cdc0a8ca2
         
     }
 
