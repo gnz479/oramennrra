beb56aec3975268779a0c844b71410f879232bc7
gnz479 <1gonzamessi@gmail.com>
2025-03-05 10:49:29 -0600

    Se uso hostinger para tratar la data y solo usar el controlador para consultar la url y crear los csv

--- app/Http/Controllers/Admin/ExcelController.php
+++ app/Http/Controllers/Admin/ExcelController.php
@@ -4,15 +4,17 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\{Storage, Http};
 use League\Csv\Writer;
 
 class ExcelController extends Controller
 {
     //funcion para generar archivos csv
-    public function generateExcel()
+    public function generateExcel2()
     {   
-        ini_set('memory_limit', '512M');
+        ini_set('memory_limit', '1024M');
+        set_time_limit(300);
+        
         $db = ['budxpzx4g', 'budxp2nvi', 'budxp862p', 'budxt88sw', 'budxyyjmm', 'budxy5i8y'];
         $query = '';
         $clist = [];
@@ -55,6 +57,7 @@ class ExcelController extends Controller
                     $urls_download[] = route('download.csv', ['filename' => $db_item]);
                     \Log::info('Excel '.$db_item.' generado. -');
                 }
+                break;
             }       
             
             return response()->json([
@@ -63,6 +66,46 @@ class ExcelController extends Controller
             ]);
 
         } catch (\Exception $e) {
+            \Log::error('Excepción al generar el CSV: ' . $e->getMessage());
+            return response()->json([
+                'status' => 'error 204',
+            ]);
+        }
+
+    }
+
+    public function generateExcel(){
+        ini_set('memory_limit', '1024M');
+        set_time_limit(300);
+        
+        try {
+            $url = "https://tciconsultoria.com/zelestra/dropletOramennrra.php";
+            $db = ['budxpzx4g', 'budxp2nvi', 'budxp862p', 'budxt88sw', 'budxyyjmm', 'budxy5i8y'];
+
+            $response = Http::get($url);
+            
+            if ($response->successful()) {
+                $data = json_decode($response->body(), true);
+
+                $con = 0;
+                foreach($data['header'] as $db_item){
+                    $csv = Writer::createFromString('');
+                    $csv->insertOne($db_item);
+                    $csv->insertAll($data['body'][$con]);
+                    
+                    $filename = 'Excel/'.$db[$con].'.csv';
+                    Storage::put($filename, $csv->getContent());
+                    $urls_download[] = route('download.csv', ['filename' => $db[$con]]);
+                    $con++;
+                }
+                \Log::info('Excel generado. -');
+                
+                return response()->json([
+                    'status' => 'ok',
+                    'urls_download' => $urls_download ?? [],
+                ]);
+            }
+        }catch (\Exception $e) {
             \Log::error('Excepción al generar el CSV: ' . $e->getMessage());
             return response()->json([
                 'status' => 'error 204',
