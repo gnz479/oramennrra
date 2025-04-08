b1e1afd55e8b662f8ec14d0d392b9d70607ca043
gnz479 <1gonzamessi@gmail.com>
2025-04-08 11:58:13 -0600

    Cambios para generar excel

--- app/Http/Controllers/Admin/ExcelController.php
+++ app/Http/Controllers/Admin/ExcelController.php
@@ -8,73 +8,58 @@ use Illuminate\Support\Facades\{Storage, Http};
 use League\Csv\Writer;
 
 class ExcelController extends Controller
-{
-    //funcion para generar archivos csv
-    public function generateExcel2()
-    {   
+{   
+
+    public function generateExcel()
+    {
         ini_set('memory_limit', '1024M');
         set_time_limit(300);
-        
-        $db = ['budxpzx4g', 'budxp2nvi', 'budxp862p', 'budxt88sw', 'budxyyjmm', 'budxy5i8y'];
-        $query = '';
-        $clist = [];
 
         try {
-            $urls_download = [];
-            foreach($db as $db_item){
-                $data = $this->getQuickbase($db_item, $query, $clist);
-                $data_excel = [];
-
-                if(count($data->data)){
-
-                    $headers = array_column(json_decode(json_encode($data->fields), true), 'label', 'id');
-                    
-                    // $headers = []; //cabecera columnas
-                    // $headers = array_map(function ($field) {
-                    //     return $field->label ?? $field->id;
-                    // }, $data->fields);
-
-                    $body = [];                    
-                    // Convertir datos en un formato plano
-                    foreach (array_chunk($data->data, 500) as $chunk) {
-                        $body = array_map(function ($item) {
-                            return array_map(function ($field) {
-                                if ($field instanceof stdClass) {
-                                    $field = (array) $field;
-                                }
-
-                                return $field->value;
-                            }, (array) $item);
-                        }, $data->data);
-                    }
-                                        
-                    $csv = Writer::createFromString('');
-                    $csv->insertOne($headers);
-                    $csv->insertAll($body);
-                    
-                    $filename = 'Excel/'.$db_item.'.csv';
-                    Storage::put($filename, $csv->getContent());
-                    $urls_download[] = route('download.csv', ['filename' => $db_item]);
-                    \Log::info('Excel '.$db_item.' generado. -');
-                }
-                break;
-            }       
+            $urlBase = "https://tciconsultoria.com/zelestra/dropletOramennrra_new.php";
+            // $urlBase = "https://tciconsultoria.com/zelestra/dropletOramennrra.php";
+            $dbList = ['budxpzx4g','budxp862p','budxq7yuj','budxt88sw','budxyyjmm','budxy5i8y','budxzg6cz','budx7h949','bud2t7c8m','buqx9twu3','buq8cu39j','buv2xwba2'];
             
+            $urls_download = [];
+
+            foreach ($dbList as $table) {
+                $response = Http::timeout(60)->get($urlBase, ['table' => $table]);
+
+                if ($response->successful()) {
+                    $data = json_decode($response->body(), true);
+                    
+                    if (!empty($data['header'][0]) && !empty($data['body'][0])) {
+                        $csv = Writer::createFromString('');
+                        $csv->insertOne($data['header'][0]); // Solo hay un header por respuesta
+                        $csv->insertAll($data['body'][0]);   // Solo un bloque de body
+
+                        $filename = 'Excel/' . $table . '.csv';
+                        Storage::put($filename, $csv->getContent());
+
+                        $urls_download[] = route('download.csv', ['filename' => $table]);
+                        \Log::info('Excel generado exitosamente. -'.$table);
+                    }
+                } else {
+                    \Log::warning("Fallo la petición de la tabla: $table");
+                }
+
+                // Puedes agregar un pequeño delay para no saturar Quickbase
+                usleep(500000); // 0.5 segundos
+            }
+
             return response()->json([
                 'status' => 'ok',
-                'urls_download' => $urls_download ?? '*',
+                'urls_download' => $urls_download,
             ]);
-
         } catch (\Exception $e) {
-            \Log::error('Excepción al generar el CSV: ' . $e->getMessage());
+            \Log::error('Excepción al generar los CSVs: ' . $e->getMessage());
             return response()->json([
                 'status' => 'error 204',
             ]);
         }
-
     }
 
-    public function generateExcel(){
+    public function generateExcel_original(){
         ini_set('memory_limit', '1024M');
         set_time_limit(300);
         
